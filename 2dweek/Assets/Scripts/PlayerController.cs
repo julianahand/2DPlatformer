cda3dcc9e8bc5cb91619f719fb9461850c0f0d88
julianahand <77911991+julianahand@users.noreply.github.com>
2021-12-13 18:48:02 -0500

    setup player

---
+++ 2dweek/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rigidBody2D;
+
+    public float runSpeed = 5;
+    public float jumpSpeed = 200f;
+
+    public SpriteRenderer spriteRenderer;
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            int levelMask = LayerMask.GetMask("Level");
+
+            if (Physics2D.BoxCast(transform.position, new Vector2(1, 0.1f), 0f, Vector2.down, 0.01f, levelMask))
+            {
+                Jump();
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        Vector2 direction = new Vector2(horizontalInput * runSpeed * Time.deltaTime, 0);
+
+        rigidBody2D.AddForce(direction);
+
+    }
+
+    void Jump()
+    {
+        rigidBody2D.velocity = new Vector2(rigidBody2D.velocity.x, jumpSpeed);
+    }
+}
